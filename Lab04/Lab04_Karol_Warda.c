0ffcc730447e74df4902965e8d07e36e0576b576
Potceax <karx132@gmail.com>
2021-11-14 17:31:01 +0100

    Finished Lab04.c file

--- Lab04/Lab04_Karol_Warda.c
+++ Lab04/Lab04_Karol_Warda.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <math.h>
+#define PI 3.14159265358979323846
 
 typedef enum { my_false = 0, my_true = !my_false } bool;
 
 typedef enum  {is_in, is_equal, is_out} circle_enum;
 typedef enum {I_quarter, II_quarter, III_quarter, IV_quarter, origin_quarter } quarter_enum;
-typedef enum {x_axis, y_axis, xy_axis} axis_enum;
+typedef enum {x_axis, y_axis, xy_axis, none_axis} axis_enum;
+typedef enum {is_higher, is_lower, is_func, is_notFunc } func_deter_enum;	// enum for function - point values
 
 typedef struct 
 {
-	int x;
-	int y;
+	double x;
+	double y;
 
 }Point;
 
@@ -41,14 +43,43 @@ int GetIntNumber(char* text, char* format)
 	}
 }
 
-int DistanceFromTheCircle(Point point)
+double GetDoubleNumber(char* text)
 {
-	return(pow(point.x, 2) + pow(point.y, 2));
+	double num;
+	while (my_true) {
+		bool isCorrect = my_true;
+
+		printf("%s", text);
+		if (!scanf_s("%lf", &num) /*&& getchar() != '\n'*/) {
+			printf("Error - Passed value is not a double type\n");
+			isCorrect = !isCorrect;
+		}
+		while (getchar() != '\n');		// to clear out the buffer
+
+		if (isCorrect)
+			return num;
+	}
 }
 
-circle_enum IsInTheCircle(Point point, int radious)
+Point GetPoint()
 {
-	int distance = DistanceFromTheCircle(point);
+	Point p;
+
+	printf("Please enter\n");
+	p.x = GetDoubleNumber("Point x value: ");
+	p.y = GetDoubleNumber("Point y value: ");
+
+	return p;
+}
+
+double DistanceFromTheCircle(Point initPoint, Point point)
+{
+	return(sqrt(pow((point.x - initPoint.x), 2) + pow((point.y - initPoint.y), 2)));
+}
+
+circle_enum IsInTheCircle(Point initPoint, Point point, double radious)
+{
+	double distance = DistanceFromTheCircle(initPoint, point);
 
 	if (distance < radious)
 		return is_in;
@@ -60,6 +91,22 @@ circle_enum IsInTheCircle(Point point, int radious)
 		return is_out;
 }
 
+void PrintPointCircle(Point initPoint, Point point, double radious)
+{
+	circle_enum result = IsInTheCircle(initPoint, point, 1.0);
+
+	switch (result) {
+		case is_in:
+			printf("Point (%.2f,% .2f) is in the circle\n\n", point.x, point.y);
+			break;
+		case is_equal:
+			printf("Point(%.2f,% .2f) is on the circle\n\n", point.x, point.y);
+			break;
+		case is_out:
+			printf("Point(%.2f,% .2f) does not belong to the circle\n\n", point.x, point.y);
+			break;
+	}
+}
 
 /*
 	x > 0, y > 0 I
@@ -74,17 +121,19 @@ quarter_enum GetPointQuarter(Point point)
 	// Made to nullify any wrong result for 0,0 points
 	if (point.x == 0 && point.y == 0)
 		return origin_quarter;
+	else if ((point.x == 0 && point.y != 0) || (point.x != 0 && point.y == 0))
+		return origin_quarter;
 
-	if (point.x >= 0 && point.y >= 0)
+	if (point.x > 0 && point.y > 0)
 		return I_quarter;
 
-	if (point.x <= 0 && point.y >= 0)
+	if (point.x < 0 && point.y > 0)
 		return II_quarter;
 
-	if (point.x <= 0 && point.y <= 0)
+	if (point.x < 0 && point.y < 0)
 		return III_quarter;
 
-	if (point.x >= 0 && point.y <= 0)
+	if (point.x > 0 && point.y < 0)
 		return IV_quarter;
 }
 
@@ -94,30 +143,33 @@ void PrintPointQuarter(Point point)
 
 	switch (quarter) {
 	case origin_quarter:
-		printf("Point (%d,% d) belong to the origin quarter\n", point.x, point.y);
+		printf("Point (%.2f,% .2f) belong to the origin quarter\n", point.x, point.y);
+		break;
 	case I_quarter:
-		printf("Point (%d,% d) belong to the I quarter\n", point.x, point.y);
+		printf("Point (%.2f,% .2f) belong to the I quarter\n", point.x, point.y);
 		break;
 	case II_quarter:
-		printf("Point (%d,% d) belong to the II quarter\n", point.x, point.y);
+		printf("Point (%.2f,% .2f) belong to the II quarter\n", point.x, point.y);
 		break;
 	case III_quarter:
-		printf("Point (%d,% d) belong to the III quarter\n", point.x, point.y);
+		printf("Point (%.2f,% .2f) belong to the III quarter\n", point.x, point.y);
 		break;
 	case IV_quarter:
-		printf("Point (%d,% d) belong to the IV quarter\n", point.x, point.y);
+		printf("Point (%.2f,% .2f) belong to the IV quarter\n", point.x, point.y);
 		break;
 	}
 }
 
 axis_enum GetPointAxis(const Point point)
 {
+	if (point.x != 0 && point.y != 0)
+		return none_axis;
 	if (point.x == 0 && point.y == 0)
 		return xy_axis;
 	if (point.x == 0 && point.y != 0)
-		return x_axis;
-	if (point.x != 0 && point.y == 0)
 		return y_axis;
+	if (point.x != 0 && point.y == 0)
+		return x_axis;
 }
 
 void PrintPointAxis(Point point)
@@ -125,14 +177,17 @@ void PrintPointAxis(Point point)
 	axis_enum axis = GetPointAxis(point);
 
 	switch (axis) {
+	case none_axis:
+		printf("Point (%.2f,% .2f) does not belong to any axis\n\n", point.x, point.y);
+		break;
 	case x_axis:
-		printf("Point (%d,% d) belong to the x axis\n\n", point.x, point.y);
+		printf("Point (%.2f,% .2f) belong to the x axis\n\n", point.x, point.y);
 		break;
 	case y_axis:
-		printf("Point (%d,% d) belong to the y axis\n\n", point.x, point.y);
+		printf("Point (%.2f,% .2f) belong to the y axis\n\n", point.x, point.y);
 		break;
 	case xy_axis:
-		printf("Point (%d,% d) belong to the x and y axis\n\n", point.x, point.y);
+		printf("Point (%.2f,% .2f) belong to the x and y axis\n\n", point.x, point.y);
 		break;
 	}
 }
@@ -143,10 +198,54 @@ void PrintPointQuarterAndAxis(Point point)
 	PrintPointAxis(point);
 }
 
+double f(double x)
+{
+	return sqrt(fabs(acos(x) + (exp(x) / log(x))));
+}
+
+
+// Determine criteria for the point argument in regards to f(point.x)
+func_deter_enum GetFunctionDetermination(Point point)
+{
+	if (point.x <= 0 || point.x >= 1)	// case where x doesn't belong to field of f(x)
+		return is_notFunc;
+
+	Point function = { point.x, f(point.x) };
+	if (point.y == function.y)
+		return is_func;
+	if (point.y > function.y)
+		return is_higher;
+	if (point.y < function.y)
+		return is_lower;
+}
+
+// Prints in what criteria does the point argument fall under
+void PrintFunctionDetermination(Point point)
+{
+	func_deter_enum fc_deter = GetFunctionDetermination(point);
+
+	switch (fc_deter){
+	case is_notFunc:
+		printf("X value of point (%.2f,% .2f) doesn't belong to the function field\n\n", point.x, point.y);
+		break;
+	case is_func:
+		printf("Point (%.2f,% .2f) is the point of function\n\n", point.x, point.y);
+		break;
+	case is_higher:
+		printf("Point (%.2f,% .2f) is higher than function\n\n", point.x, point.y);
+		break;
+	case is_lower:
+		printf("Point (%.2f,% .2f) is lower than function\n\n", point.x, point.y);
+		break;
+	}
+}
+
+
+
 void Zad25(char c)
 {
 	// Checks if c is a letter
-	if ((c < 'a' || c > 'z') || (c < 'A' || c > 'Z'))
+	if ((c < 'a' || c > 'z') && (c < 'A' || c > 'Z'))
 		return;
 	
 	c = ToSmaller(c);
@@ -158,7 +257,7 @@ void Zad25(char c)
 	char input = getchar();
 	while (getchar() != '\n');
 	
-	input = ToSmaller(input);
+	//input = ToSmaller(input);		// When program requiers only small case letters
 
 	if (input == c)
 		printf("Input is equal to %c\n\n", c);
@@ -168,53 +267,32 @@ void Zad25(char c)
 	printf("Koniec Programu\n\n\n");
 }
 
-void Zad26(const Point initPoint, const int radious)
+void Zad26(const Point initPoint, const double radious)
 {
-	printf("Program which tells user if point he wrote belongs to the point (%d,% d) and radious %d.\n",
+	printf("Program which tells user if the point he wrote belongs to the circle with center point (%.2f,% .2f) and radious %.2f.\n",
 		initPoint.x, initPoint.y, radious);
 	printf("Autor: Karol Warda\n\n");
 
-	Point input;
+	Point input = GetPoint();
 
-	printf("Please enter\n");
-	input.x = GetIntNumber("Point x value: ", "%d");
-	input.y = GetIntNumber("Point y value: ", "%d");
-
-	circle_enum result = IsInTheCircle(input, 1);
+	circle_enum result = IsInTheCircle(initPoint, input, radious);
 
 	if (result == is_in || result == is_equal)
-		printf("Point (%d,% d) belong to the circle\n\n", input.x, input.y);
+		printf("Point (%.2f,% .2f) belong to the circle\n\n", input.x, input.y);
 	else
-		printf("Point (%d,% d) does not belong to the circle\n\n", input.x, input.y);
+		printf("Point (%.2f,% .2f) does not belong to the circle\n\n", input.x, input.y);
 
 	printf("Koniec Programu\n\n\n");
 }
 
-void Zad27(const Point initPoint, const int radious)
+void Zad27(const Point initPoint, double radious)
 {
-	printf("Program which tells user if point he wrote belongs to the point (%d,% d) and radious %d.\n",
+	printf("Program which tells user if the point he wrote belongs to the circle with center point (%.2f,% .2f) and radious %.2f.\n",
 		initPoint.x, initPoint.y, radious);
 	printf("Autor: Karol Warda\n\n");
 
-	Point input;
-
-	printf("Please enter\n");
-	input.x = GetIntNumber("Point x value: ", "%d");
-	input.y = GetIntNumber("Point y value: ", "%d");
-
-	circle_enum result = IsInTheCircle(input, 1);
-
-	switch (result) {
-	case is_in:
-		printf("Point (%d,% d) is in the circle\n\n", input.x, input.y);
-		break;
-	case is_equal:
-		printf("Point(%d,% d) is on the circle\n\n", input.x, input.y);
-		break;
-	case is_out:
-		printf("Point(%d,% d) does not belong to the circle\n\n", input.x, input.y);
-		break;
-	}
+	Point input = GetPoint();
+	PrintPointCircle(initPoint, input, radious);
 
 	printf("Koniec Programu\n\n\n");
 }
@@ -224,12 +302,7 @@ void Zad28()
 	printf("Program which tells user in what quarter his imput point belongs\n");
 	printf("Autor: Karol Warda\n\n");
 
-	Point input;
-
-	printf("Please enter\n");
-	input.x = GetIntNumber("Point x value: ", "%d");
-	input.y = GetIntNumber("Point y value: ", "%d");
-
+	Point input = GetPoint();
 	PrintPointQuarter(input);
 
 	printf("Koniec Programu\n\n\n");
@@ -240,12 +313,7 @@ void Zad29()
 	printf("Program which tells user in what quarter and axis his imput point belongs\n");
 	printf("Autor: Karol Warda\n\n");
 
-	Point input;
-
-	printf("Please enter\n");
-	input.x = GetIntNumber("Point x value: ", "%d");
-	input.y = GetIntNumber("Point y value: ", "%d");
-
+	Point input = GetPoint();
 	PrintPointQuarterAndAxis(input);
 
 	printf("Koniec Programu\n\n\n");
@@ -253,18 +321,71 @@ void Zad29()
 
 void Zad30()
 {
+	printf("Program which gets float input:\n");
+	printf("If it is an integer, it will tell about it to user.\n");
+	printf("If not the number will be rounded to lower closest integer\n\n");
+	printf("Autor: Karol Warda\n\n\n");
 
+	double num = GetDoubleNumber("Please enter a number: ");
+
+	num = roundf(num * 100) / 100;	// rounds input to 2 decimals
+
+	if (num == floor(num))
+		printf("The number %.2f is an integer\n\n", num);
+	else
+		printf("The number %.2f wasn't an integer. The rounded value is equal to %.2f\n\n", num, floor(num));
+
+	printf("Koniec Programu\n\n\n");
+}
+
+void Zad31()
+{
+	printf("Program which gets point input from user and determine certain criteria about f(x):\n");
+	printf("f(x) = sqrt(abs(arccos(x) + (exp(x)/ln(x)))\n\n");
+	printf("Autor: Karol Warda\n\n\n");
+
+	Point input = GetPoint();	
+	PrintFunctionDetermination(input);
+
+	printf("Koniec Programu\n\n\n");
+}
+
+void Zad32()
+{
+	printf("Autor: Karol Warda\n\n");
+
+	int x;
+	printf("Testowanie instr. przypisania.\n\n");
+	x = 3;
+	printf("Po x=3; wart. x jest: %d\n", x);
+	x = 5;
+	printf("Po x=5; wart. x jest: %d\n", x);
+	x += 2;
+	printf("Po x=x+2; wart. x jest: %d\n", x);
+	x *= (x + 1);
+	printf("Po x=x*(x+1); wart. x jest: %d\n", x);
+	x += 1;
+	printf("Po x=x+1; wart. x jest: %d\n", x);
+	x -= (3 / 4);
+	printf("Po x=x-(3/4); wart. x jest: %d\n", x);
+	x /= (x + 1);
+	printf("Po x=x/(x+1); wart. x jest: %d\n", x);
+	x -= 1;
+	printf("Po x--; wart. x jest: %d\n", x);
+	printf("\n\nKoniec programu. \n");
 }
 
 int main()
 {
-	Zad25('2');
+	Zad25('T');
 
-	const Point initPoint = { 0,0 };
-	const int initRad = 1;
-	/*Zad26(initPoint, initRad);
-	Zad27(initPoint, initRad);*/
-	/*Zad28();
-	Zad29();*/
-	
+	const Point initPoint = { 0.0, 0.0 };
+	const double initRad = 1.0;
+	Zad26(initPoint, initRad);
+	Zad27(initPoint, initRad);
+	Zad28();
+	Zad29();
+	Zad30();
+	Zad31();
+	Zad32();
 }
